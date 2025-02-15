7273d27fe140924725f86bd24bf9168bd8b74ac6
kei0710KEI <keisuke0710.taka@gmail.com>
2025-02-16 01:25:36 +0900

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
-import { dirname } from "path";
+import { dirname } from "path"; 
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // `any` 型の使用を許可
+      "@typescript-eslint/no-unused-vars": "off",  // 未使用変数の警告を無効化
+    },
+  },
 ];
 
 export default eslintConfig;
